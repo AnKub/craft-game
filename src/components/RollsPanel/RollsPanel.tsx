dfcdb4791c9ac185845114941c7246be8980b569
AnKub <ankub3088@gmail.com>
2025-06-29 11:17:22 +0300

    bambarda maxima))

--- src/components/RollsPanel/RollsPanel.tsx
+++ src/components/RollsPanel/RollsPanel.tsx
@@ -1,35 +1,35 @@
-// import './RollsPanel.scss'
+import './RollsPanel.scss'
 
-// type RollsPanelProps = {
-//   rolls: number
-//   maxRolls: number
-//   timer: number // у секундах
-// }
+type RollsPanelProps = {
+  rolls: number
+  maxRolls: number
+  timer: number // у секундах
+}
 
-// const formatTime = (sec: number) => {
-//   const m = Math.floor(sec / 60).toString().padStart(2, '0')
-//   const s = (sec % 60).toString().padStart(2, '0')
-//   return `${m}:${s}`
-// }
+const formatTime = (sec: number) => {
+  const m = Math.floor(sec / 60).toString().padStart(2, '0')
+  const s = (sec % 60).toString().padStart(2, '0')
+  return `${m}:${s}`
+}
 
-// const RollsPanel = ({ rolls, maxRolls, timer }: RollsPanelProps) => (
-//   <div className="rolls-panel">
-//     <div className="rolls-label">
-//       Available rolls
-//       <span className="rolls-count">{rolls}/{maxRolls}</span>
-//     </div>
-//     <div className="rolls-bar-bg">
-//       {Array.from({ length: maxRolls }).map((_, i) => (
-//         <div
-//           key={i}
-//           className={`rolls-bar-section${i < rolls ? '' : ' empty'}`}
-//         />
-//       ))}
-//     </div>
-//     {rolls < maxRolls && (
-//       <div className="rolls-timer">{formatTime(timer)}</div>
-//     )}
-//   </div>
-// )
+const RollsPanel = ({ rolls, maxRolls, timer }: RollsPanelProps) => (
+  <div className="rolls-panel">
+    <div className="rolls-label">
+      Available rolls
+      <span className="rolls-count">{rolls}/{maxRolls}</span>
+    </div>
+    <div className="rolls-bar-bg">
+      {Array.from({ length: maxRolls }).map((_, i) => (
+        <div
+          key={i}
+          className={`rolls-bar-section${i < rolls ? '' : ' empty'}`}
+        />
+      ))}
+    </div>
+    {rolls < maxRolls && (
+      <div className="rolls-timer">{formatTime(timer)}</div>
+    )}
+  </div>
+)
 
-// export default RollsPanel
+export default RollsPanel
